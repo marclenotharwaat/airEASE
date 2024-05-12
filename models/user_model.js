fa965bc2a64bd9426e4e939296b4186908d01693
Mohamedahmedeid <96028635+Mohamedahmedeid@users.noreply.github.com>
2024-05-12 14:23:13 +0300

    Add files via upload

--- models/user_model.js
+++ models/user_model.js
@@ -1,50 +1,67 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const userRole = require('../utility/userRole')
-const userSchema = new mongoose.Schema(
-    {
-        firstName: {
+const userRole = require('../utility/userRole');
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String, 
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: [validator.isEmail, "field must be a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    token: {
+        type: String
+    },
+    role: {
+        type: String,
+        enum: [userRole.USER, userRole.ADMIN],
+        default: userRole.USER
+    },
+    resetPasswordToken: {
+        type: String
+    },
+    resetPasswordExpires: {
+        type: Date
+    },
+    country : {
+        type: String
+    },
+    city : {
+    type : String
+    },
+        nationality: {
             type: String,
             require: true,
-            trim: true
+           
         },
-        lastName: {
-            type: String, 
-            require: true,
-            trim: true
-        },
-        email: {
+        PassportNumber: {
             type: String,
             require: true,
-            unique: true,
-            trim: true,
-            validate: [validator.isEmail, "filed must be a valid email address"]
+           
         },
-        password: {
-            type: String,
-            require: true,
-            trim: true
-        },
-        token: {
-            type: String
-        },
-        role: {
-            type: String,
-            enum: [userRole.USER, userRole.ADMIN],
-            default: userRole.USER
-        },
+        search : {
+            from : {type: String},
+            to : {type: String}
+        }
+    }
+)
         /*----------------------------------------------------------*/
         // address: {
-        //     type: String,
-        //     require: true,
-           
-        // },
-        // nationality: {
-        //     type: String,
-        //     require: true,
-           
-        // },
-        // PassportNumber: {
         //     type: String,
         //     require: true,
            
@@ -54,8 +71,18 @@ const userSchema = new mongoose.Schema(
         //     require: true,
         //    /*----------------------------------------------------------*/
         // },
-    }
-)
+
+userSchema.methods.generateResetToken = function() {
+    // Generate a random token
+    const token = require('crypto').randomBytes(2).toString('hex');
+    // Set the reset password token
+    this.resetPasswordToken = token;
+    // Set the expiration time to 1 hour from now
+    this.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    return token;
+};
+
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
